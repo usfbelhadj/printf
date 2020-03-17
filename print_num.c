95378137ae750d861a47583f749e08e61abf2152
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-17 23:18:28 +0000

    teest

--- print_num.c
+++ print_num.c
@@ -10,26 +10,97 @@
  */
 int print_number(va_list arg)
 {
-int n, div, tens = 1, len = 0;
-n = va_arg(arg, int);
-if (n < 0)
+  int nb, ddiv, num = 1, len = 0;
+
+  nb = va_arg(arg, int);
+  if (nb < 0)
+    {
+      _putchar('-');
+      len++;
+      nb = -1 * nb;
+    }
+  ddiv = nb;
+  while ((ddiv / 10) != 0)
+    {
+      ddiv = ddiv / 10;
+      num = num * 10;
+    }
+  while (num > 0)
+    {
+      _putchar((nb / num) + '0');
+      len++;
+      nb = nb % num;
+      num = num / 10;
+    }
+  return (len);
+}#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int print_number(va_list arg)
 {
-_putchar('-');
-len++;
-n = -n;
-}
-div = n;
-while ((div / 10) != 0)
+  int nb, ddiv, num = 1, len = 0;
+
+  nb = va_arg(arg, int);
+  if (nb < 0)
+    {
+      _putchar('-');
+      len++;
+      nb = -1 * nb;
+    }
+  ddiv = nb;
+  while ((ddiv / 10) != 0)
+    {
+      ddiv = ddiv / 10;
+      num = num * 10;
+    }
+  while (num > 0)
+    {
+      _putchar((nb / num) + '0');
+      len++;
+      nb = nb % num;
+      num = num / 10;
+    }
+  return (len);
+}#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int print_number(va_list arg)
 {
-div /= 10;
-tens *= 10;
-}
-while (tens > 0)
-{
-_putchar((n / tens) + '0');
-len++;
-n %= tens;
-n /= 10;
-}
-return (len);
+  int nb, ddiv, num = 1, len = 0;
+
+  nb = va_arg(arg, int);
+  if (nb < 0)
+    {
+      _putchar('-');
+      len++;
+      nb = -1 * nb;
+    }
+  ddiv = nb;
+  while ((ddiv / 10) != 0)
+    {
+      ddiv = ddiv / 10;
+      num = num * 10;
+    }
+  while (num > 0)
+    {
+      _putchar((nb / num) + '0');
+      len++;
+      nb = nb % num;
+      num = num / 10;
+    }
+  return (len);
 }
