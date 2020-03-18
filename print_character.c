86f96b4b84a2ea46c1c86bbc8b1710cd6bf7ea96
usfbelhadj <usf.belhadj@gmail.com>
2020-03-18 15:25:17 +0100

    Fiix

--- print_character.c
+++ print_character.c
@@ -3,15 +3,14 @@
 #include <stdarg.h>
 #include <unistd.h>
 /**
- *
- *
- *
- *
+ *print_char- Fuction That Print Char
+ *@arg: Inpit
+ *Return: Output
  */
 int print_char(va_list arg)
 {
 char c;
 c = va_arg(arg, int);
 _putchar(c);
-return(1);
+return (1);
 }
