7e1117128b766c52162b768bcce1e8ecea3ab034
usfbelhadj <usf.belhadj@gmail.com>
2020-03-18 15:22:55 +0100

    Fiix

--- print_num.c
+++ print_num.c
@@ -3,34 +3,33 @@
 #include <stdarg.h>
 #include <unistd.h>
 /**
- *
- *
- *
- *
+ *print_number- Function That Print Numbers
+ *@arg: Input
+ *Return: integer
  */
 int print_number(va_list arg)
 {
-  int long nb, num = 1, len = 0;
-  unsigned int long ddiv;
-  nb = va_arg(arg, int);
-  if (nb < 0)
-    {
-      _putchar('-');
-      len++;
-      nb = -1 * nb;
-    }
-  ddiv = nb;
-  while ((ddiv / 10) != 0)
-    {
-      ddiv = ddiv / 10;
-      num = num * 10;
-    }
-  while (num > 0)
-    {
-      _putchar((nb / num) + '0');
-      len++;
-      nb = nb % num;
-      num = num / 10;
-    }
-  return (len);
+int long nb, num = 1, len = 0;
+unsigned int long ddiv;
+nb = va_arg(arg, int);
+if (nb < 0)
+{
+_putchar('-');
+len++;
+nb = -1 * nb;
+}
+ddiv = nb;
+while ((ddiv / 10) != 0)
+{
+ddiv = ddiv / 10;
+num = num * 10;
+}
+while (num > 0)
+{
+_putchar((nb / num) +'0');
+len++;
+nb = nb % num;
+num = num / 10;
+}
+return (len);
 }
